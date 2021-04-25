0b65a9fd6862422d7b01604a642e3593e9ad6151
Elliot Blanchard <git@elliotblanchard.com>
2021-04-25 12:35:22 -0400

    Implemented basic factorial

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Testing and R+D for extension + WASM + local storage pipeline
 https://developer.chrome.com/docs/extensions/mv3/manifest/web_accessible_resources/
 https://github.com/elliotblanchard/Extension-with-WASM/blob/master/manifest.json
 https://github.com/jedisct1/libsodium.js/issues/196
+https://krasimirtsonev.com/blog/article/Chrome-extension-debugging-dev-tools-tab-or-how-to-make-console-log
+https://github.com/hackcasual/ChromeWASMCSP
--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     const setLocalStorage = document.getElementById('setLocalStorage')
     const getLocalStorage = document.getElementById('getLocalStorage')
-    const runWASM = document.getElementById('runWASM')
+    const runHelloWorld = document.getElementById('runHelloWorld')
+    const runFactorial = document.getElementById('runFactorial')
 
     setLocalStorage.addEventListener('click', function() {
         const inputValue = document.getElementById('localStorage').value
         localStorage.setItem('extensionTest', inputValue)
-        alert(`Local storage set to ${localStorage.getItem('extensionTest')}`)
-    });
+        document.getElementById('localStorageContent').textContent = localStorage.getItem('extensionTest')
+    })
 
     getLocalStorage.addEventListener('click', function() {
-        alert(`Local storage set to ${localStorage.getItem('extensionTest')}`)
-    });    
+        document.getElementById('localStorageContent').textContent = localStorage.getItem('extensionTest')
+    })   
 
-    runWASM.addEventListener('click', function() {     
-        fetchAndInstantiate()
-    });     
+    runHelloWorld.addEventListener('click', function() {     
+        fetchAndInstantiateHelloWorld()
+    })  
+    
+    runFactorial.addEventListener('click', function() {     
+        fetchAndInstantiateFactorial()
+    })     
 });
 
-async function fetchAndInstantiate() {
+async function fetchAndInstantiateHelloWorld() {
 
     const response = await fetch('./main.wasm')
     const buffer = await response.arrayBuffer()
-    // Apprently 'unsafe-eval' must be set in manifest 
+    // Apprently 'wasm-eval' must be set in manifest 
     // for WebAssembly.instantiate to execute in Chrome extension
     const obj = await WebAssembly.instantiate(buffer) 
     const output = new Uint8Array(obj.instance.exports.memory.buffer)
@@ -35,8 +40,18 @@ async function fetchAndInstantiate() {
       string += String.fromCharCode(output[i])
     }  
   
-    //document.getElementById('container').textContent = string
-    
+    document.getElementById('helloWorldOutput').textContent = string  
+  }
 
-    alert(`Output is: ${string}`)    
+  async function fetchAndInstantiateFactorial() {
+
+    const response = await fetch('./factorial.wasm')
+    const buffer = await response.arrayBuffer()
+    // Apprently 'wasm-eval' must be set in manifest 
+    // for WebAssembly.instantiate to execute in Chrome extension
+    const obj = await WebAssembly.instantiate(buffer) 
+    const output = new Uint8Array(obj.instance.exports.memory.buffer)
+      
+    // Simple method is possible because factorial returns a number, not a string
+    document.getElementById('factorialOutput').textContent  = obj.instance.exports.factorial(5)    
   }
